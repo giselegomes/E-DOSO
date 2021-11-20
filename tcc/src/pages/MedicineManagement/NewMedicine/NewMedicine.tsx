762c582c2295fce5841edbb093e48d1e3db37dcd
Gisele Gomes <gselegomes@gmail.com>
2021-11-20 11:06:58 -0300

    inserindo botao de notificacao

--- tcc/src/pages/MedicineManagement/NewMedicine/NewMedicine.tsx
+++ tcc/src/pages/MedicineManagement/NewMedicine/NewMedicine.tsx
@@ -1,41 +1,124 @@
-import React from "react";
-import { ScrollView, View, TextInput, StatusBar } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-// import RNPickerSelect from 'react-native-picker-select';
+import React, { useState, useEffect, useRef } from 'react';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import { Text, View, Button, Platform, ScrollView, TextInput, StatusBar } from 'react-native';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
 
 const NewMedicine = () => {
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+        // This listener is fired whenever a notification is received while the app is foregrounded
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
 
     return (
-        <ScrollView >
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent 
-            />
-            
-            <TextInput
-                placeholder="Nome:" 
-            />
+        <View>
+            <ScrollView >
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor="transparent"
+                    translucent
+                />
 
-            <TextInput
-                placeholder="Horário inicial:" 
-            />
+                <TextInput
+                    placeholder="Nome:"
+                />
 
-            <TextInput
-                placeholder="Repetir de:" 
-            />
+                <TextInput
+                    placeholder="Horário inicial:"
+                />
 
-            {/* <RNPickerSelect
-            
-            items={[
-                { label: 'Football', value: 'football' },
-                { label: 'Baseball', value: 'baseball' },
-                { label: 'Hockey', value: 'hockey' },
-            ]}
-        /> */}
+                <TextInput
+                    placeholder="Repetir de:"
+                />
+            </ScrollView>
 
-        </ScrollView>
+            <View>
+                <Button
+                    title="Aperta aqui"
+                    onPress={async () => {
+                        await sendPushNotification(expoPushToken);
+                    }}
+                />
+            </View>
+        </View>
     )
+}
+
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
+async function sendPushNotification(expoPushToken) {
+    const message = {
+        to: expoPushToken,
+        sound: 'default',
+        title: 'Original Title',
+        body: 'And here is the body!',
+        data: { someData: 'goes here' },
+    };
+
+    await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Accept-encoding': 'gzip, deflate',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+    });
+}
+
+async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+    return token;
 }
 
 export default NewMedicine;
